f9acc1175a440fd814ba50e76c642e30662770dd
YiJie <yijie4188@gmail.com>
2024-10-13 16:57:01 +0800

    Simplify code

--- package-a/src/index.ts
+++ package-a/src/index.ts
@@ -1,5 +1,3 @@
-import type { B, C } from 'package-a'
-
 import { foo } from './foo'
 import { bar } from './bar'
 
@@ -11,6 +9,3 @@ export function getFoo (): A {
 export function getBar (): A {
   return bar
 }
-
-export const b: B = 1
-export const c: C = 2
